aba3b72913b396a4ccf8a470dc7372511f3a0554
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-05-10 19:31:13 +0300

    HTA Opener(HTML Opener)

---
+++ Source Code/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTAOpener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OpenFileDialog x = new OpenFileDialog();
+            x.Title = "Open HTML Files";
+            x.Filter = "html files (*.html)|*.html";
+            if (x.ShowDialog() == DialogResult.OK)
+            {
+                Process.Start("mshta", x.FileName);
+            }
+            Environment.Exit(0);
+        }
+    }
+}
